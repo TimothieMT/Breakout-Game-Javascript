2dd92fed0609cd9988c6a6556c3f1e0e2e65b087
TimothieMT <ttolk89@gmail.com>
2022-09-19 23:44:18 +0200

    update

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-
 //Globalevariablen
 const canvas = document.getElementById("mainCanvas")
 const context = canvas.getContext("2d")
@@ -10,10 +9,31 @@ let dy = -2
 //Ball parameter
 const ballRadius = 2
 //Balken parameter
-const barheight = 10
-const barwidht = 75
-let barX = (canvas.height - canvas.width) / 2
+const barHeight = 5
+const barWidht = 35
+let barX = (canvas.height - barWidht) / 2
+let rightPress = false
+let leftPress = false
 
+
+const keyDownHandler = (element) => {
+    if (element.key === "Right" || element.key === "ArrowRight") {
+        rightPress = true
+    } else if (element.key === "Left" || element.key === "ArrowLeft") {
+        leftPress = true
+    }
+}
+
+const keyUpHandler = (element) => {
+    if (element.key === "Right" || element.key === "ArrowRight") {
+        rightPress = false
+    } else if (element.key === "Left" || element.key === "ArrowLeft") {
+        leftPress = false
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
 
 //Funktion zum Zeichnen eines Balles
 const ball = () => {
@@ -26,7 +46,7 @@ const ball = () => {
 
 const bar = () => {
     context.beginPath()
-    context.rect(barX, canvas.height - barheight, barwidht, barheight)
+    context.rect(barX, canvas.height - barHeight, barWidht, barHeight)
     context.fillStyle = "#53382F"
     context.fill()
     context.closePath()
@@ -35,20 +55,28 @@ const bar = () => {
 const draw = () => {
     context.clearRect(0, 0, canvas.width, canvas.height)
     ball()
+    bar()
 
+    //Kollision mit Wänden
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius)
         dx = -dx
 
     if (y + dy > canvas.height - ballRadius || y + dy < ballRadius)
         dy = -dy
-
-
+   //Bewegung Balken
+    if (rightPress) {
+        barX += 7
+        if (barX + barWidht > canvas.width) {
+            barX = canvas.width - barWidht
+        }
+    } else if (leftPress) {
+        barX -= 7
+        if (barX < 0) {
+            barX = 0
+        }
+    }
     x += dx
     y += dy
 }
 
-const start = () => {
-    ball()
-    bar()
-    setInterval(draw, 30)
-}
+const interval = setInterval(draw, 20)
